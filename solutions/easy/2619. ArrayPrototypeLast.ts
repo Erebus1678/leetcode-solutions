cf946828f92891fd50b51bb80167ea35b1ac1f64
Erebus1678 <dmitryi.platov@gmail.com>
2025-03-24 09:56:45 +0200

    ts for array prototype

--- solutions/easy/2619. ArrayPrototypeLast.ts
+++ solutions/easy/2619. ArrayPrototypeLast.ts
@@ -1,12 +1,20 @@
-//@ts-nocheck
+
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
-Array.prototype.last = function () {
-  if (!this.length) return -1
-  return this[this.length - 1]
+
+declare global {
+  interface Array<T> {
+    last(): T | -1;
+  }
 }
 
+Array.prototype.last = function <T>(): T | -1 {
+  if (!this.length) return -1;
+  return this[this.length - 1];
+};
+
+export {};
 /**
  * const arr = [1, 2, 3];
  * arr.last(); // 3
