deb286d5f0658530f34bebe314f597d27c84b519
Erebus1678 <90376194+Erebus1678@users.noreply.github.com>
2025-03-25 16:10:19 +0000

    add  2677 and desc for some problems

--- solutions/easy/2619. ArrayPrototypeLast.ts
+++ solutions/easy/2619. ArrayPrototypeLast.ts
@@ -1,4 +1,14 @@
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
 
+// You may assume the array is the output of JSON.parse.
+
+ 
+
+// Example 1:
+
+// Input: nums = [null, {}, 3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
